433700622798ccb4e10775d09cc48e4cb8402629
Vahid Porki <ivahidp@gmail.com>
2022-09-14 21:15:23 +0430

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const KEY = 'c5df6086-1516-4cd1-9334-1533b94d2082';
